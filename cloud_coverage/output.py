27c9f8333d43c1470684b21d2c1ad8aa78d3d03a
sorazy <soliman.alnaizy@gmail.com>
2019-04-05 12:25:25 -0400

    Changed IHS formulas

--- cloud_coverage/output.py
+++ cloud_coverage/output.py
@@ -1,30 +1,67 @@
 # Conviniently outputs all the pyplots to a certain directory
-
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import sys
+
+def calc_hue(i, r, g, b):
+    if (r < g < b):
+        try:
+            return ((b - r) / (i - (3 * r))) + 1
+        except:
+            return 1e9
+    elif (g < b):
+        try:
+            return ((b - r) / (i - (3 * r))) + 2
+        except:
+            return 1e9
+    else:
+        try:
+            return ((b - r) / (i - (3 * r)))
+        except:
+            return 1e9
+
+
+def calc_sat(i, h, r, g, b):
+    if (h <= 1):
+        return (i - (3 * b)) / i
+    elif (1 < h <= 2):
+        return (i - (3 * r)) / i
+    else:
+        return (i - (3 * g)) / i
 
 # Load an color image in grayscale
 for i in range(1, 24):
     if i < 13:
-        img = cv2.imread('results/03282019/in/' + str(i) + '.jpg')
+        img = cv2.imread('results/04042019/in/' + str(i) + '.jpg')
     else:
-        img = cv2.imread('results/03282019/in/' + str(i) + '.png')
+        img = cv2.imread('results/04042019/in/' + str(i) + '.png')
 
+    # Exit if image was not opened (Probably due to a wrong file name)
+    if (img.all() == None):
+        print("Wrong file name/path, buddy.")
+        sys.exit()
+
+    # OpenCV opens images as GBR. We need to change it to RGB, convert it to a numpy array
+    # and then normalize all the values
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = np.asarray(img).astype(np.int)
+    img = np.asarray(img).astype(np.double)
+    img /= 255
 
-    # Using numpy to calculate Intensity, Hue, and Saturation
-    # Intensity  = 1/3 * (r + g + b) / 3
-    # Hue        = arccos(((r - g) + (r - b)) / (sqrt((r - g)^2 + (r - g)*(g - b))))
-    # Saturation = 1 - (3 / (r + g + b)) * min(r, g, b) 
-    # ins = (r + g + b) / 3
-    # num = .5 * (r + r - g - b)
-    # den = np.sqrt((r - g)**2 + (r - g)*(g - b))
-    # hue = np.arccos(num / den)
-    sat = 1 - (3 / (img[:,:,0] + img[:,:,1] + img[:,:,2])) * np.minimum(np.minimum(img[:,:,0], img[:,:,1]), img[:,:,2])
-    sat = np.where(abs(sat) > 0.15, 0, sat * 2)
+    # Vectorize the functions above so that we can use numpy to easily apply the functions
+    # to all pixels
+    v_hue = np.vectorize(calc_hue)
+    v_sat = np.vectorize(calc_sat)
+
+    # Use the vectorized functions above and apply to every element of the matrix
+    intensity = img[:,:,0] + img[:,:,1] + img[:,:,2]
+    hue = v_hue(intensity, img[:,:,0], img[:,:,1], img[:,:,2])
+    sat = v_sat(intensity, hue, img[:,:,0], img[:,:,1], img[:,:,2])
+
+    # Change values to make output prettier
+    sat = np.where(sat > 0.1, 0, sat * 2)
 
     # To make the output image grayscale
     plt.imshow(sat, cmap='gray')
-    plt.savefig('results/03282019/out/' + str(i) + 'out.png')
+    plt.savefig('results/04042019/out/out' + str(i) + '.png')
+    print("Picture #",i," is done.")
